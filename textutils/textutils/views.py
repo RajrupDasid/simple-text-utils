aca983a85ad2b07fcbb438ee68adba27fb568a5f
RajrupDasid <rajrupdasid@outlook.com>
2021-07-12 15:56:13 +0530

    Bug fixes and all switches are working together

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -13,19 +13,26 @@ def index(request):
 def about(request):
     return HttpResponse("This is about page")
 def analyze(request):
-    new=request.GET.get('text','default')
-    rp=request.GET.get('removepunc','off')
-    fc=request.GET.get('fullcaps','off')
-    lc=request.GET.get('lowercaps','off')
-    nlr=request.GET.get('nlr','off')
-    sr=request.GET.get('sr','off')
+    new=request.POST.get('text','default')
+    rp=request.POST.get('removepunc','off')
+    fc=request.POST.get('fullcaps','off')
+    lc=request.POST.get('lowercaps','off')
+    nlr=request.POST.get('nlr','off')
+    sr=request.POST.get('sr','off')
     analyzed=""
     punctuations=''' _ - + = !#@$!^$3#&# * # @( ) @ * ) @ ! * ^ $ ^ $ % @{ } [ ] " " ' ' : ; . / ? > ? < ,  '''
     if rp=='on':
         for char in new:
             if char not in punctuations:
                 analyzed= analyzed+char
-    elif(fc=='on'):
+        context={
+        "punctuations":punctuations,
+        "analyzed":analyzed,
+        'new':new,
+        }
+        new=analyzed
+        #return render(request,'analyze.html',context)
+    if(fc=='on'):
         analyzed=""
         for char in new:
             analyzed+=char.upper()
@@ -35,8 +42,9 @@ def analyze(request):
             "new":new,
             "fc":fc,
         }
-        return render(request,'analyze.html',context)
-    elif(lc=='on'):
+        new=analyzed
+        #return render(request,'analyze.html',context)
+    if(lc=='on'):
         analyzed=""
         for char in new:
             analyzed+=char.lower()
@@ -47,11 +55,12 @@ def analyze(request):
             "fc":fc,
             "lc":lc,
         }
-        return render(request,"analyze.html",context)
-    elif(nlr=="on"):
+        new=analyzed
+        #return render(request,"analyze.html",context)
+    if(nlr=="on"):
         analyzed=""
         for char in new:
-            if char !="\n":
+            if char !="\n" and  char!="\r":
                 analyzed+=char
         context={
             "punctuations":punctuations,
@@ -61,9 +70,9 @@ def analyze(request):
             "lc":lc,
             "nlr":nlr,
         }
-
-        return render(request,"analyze.html",context)
-    elif(sr=="on"):
+        new=analyzed
+        #return render(request,"analyze.html",context)
+    if(sr=="on"):
         analyzed=""
         for index,char in enumerate(new):
             if new[index] ==" " and new[index+1] ==" ":
@@ -79,13 +88,8 @@ def analyze(request):
             "nlr":nlr,
             'sr':sr,
         }
-
-        return render(request,"analyze.html",context)        
-    else :
-        return HttpResponse ("Error")
-    context={
-        "punctuations":punctuations,
-        "analyzed":analyzed,
-        'new':new,
-    }
-    return render(request,'analyze.html',context)
+        new=analyzed
+    if(rp!="on" or fc!="on" and nlr!="on" and sr!="on" and lc!="on"):
+        return HttpResponse("error")
+    return render(request,"analyze.html",context)  
+    
